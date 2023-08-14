99c969681dfc2d95f8c14e76eaa2c011260a5727
@tail-yuan <3277930931@qq.com>
2023-08-14 17:32:13 +0800

    回顾一遍

--- 链表算法/翻转局部链表.cc
+++ 链表算法/翻转局部链表.cc
@@ -14,11 +14,11 @@ public:
         dummy->next=head;
         //prev指向m的前一个节点
         ListNode * prev=dummy;
-        for(int i=0;i<m-1;i++)
+        for(int i=1;i<m-1;i++)
             prev=prev->next;
         //
         ListNode* right=prev;
-        for(int i=0;i<n-m+1;i++)
+        for(int i=1;i<n-m+1;i++)
             right=right->next;
         //截取子链表
         ListNode* left=prev->next;
@@ -47,3 +47,38 @@ public:
         }
     }
 };
+
+ListNode* Func(ListNode* head,int n,int m)
+{
+	ListNode* dummy=new ListNode(-1);
+	ListNode* prev=dummy;
+	for(int i=1;i<m-1;i++)
+		prev=prev->next;
+	ListNode* left=prev->next;//翻转之后,就是区间的最后一个节点
+	ListNode*right=prev;
+	for(int i=1;i<n-m+1;i++)
+		right=right->next;
+	//right 就是待翻转区间右端点
+	ListNode* cur=right->next;
+	//
+	prerv->next=nullptr;
+	right->next=nullptr;//对于翻转链表很关键
+	//
+	reverseList(left);
+	//
+	prev->next=right;
+	left->next=cur;
+	return head;
+}
+void reverseList(ListNode* head)
+{
+	ListNode* prev=nullptr;
+	ListNode* cur=head;
+	while(cur)
+	{
+			ListNode* next=cu->next;
+			cur->next=prev;
+			prev=cur;
+			cur=next;
+	}
+}
